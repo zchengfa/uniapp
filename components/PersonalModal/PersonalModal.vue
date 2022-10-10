f44c5901ccc1c22a07459537a4877b06accd90f8
zchengfa <1522977814@qq.com>
2022-10-10 11:15:42 +0800

    custom plugins (personal modal)

---
+++ components/PersonalModal/PersonalModal.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="personal-modal" @click.native="closeModal">
+		<view class="content">
+			<text>我是个人板块</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	
+	export default {
+		name:"PersonalModal",
+		props:{
+			user:{
+				type:Object,
+				default(){
+					return {}
+				}
+			}
+		},
+		methods:{
+			closeModal(){
+				this.$modalOut()
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+.personal-modal{
+	position: absolute;
+	top:0;
+	left: 0;
+	max-width:500px;
+	width: 100vw;
+	height: 100vh;
+	background-color: rgba(0, 0, 0, .4);
+	transform: translateX(-100%);
+	transition-duration: .3s;
+	z-index: 1000;
+	.content{
+		margin-left: 0;
+		width: 84%;
+		height: 100%;
+		background-color: #fff;
+	}
+}
+.modal-in{
+	left: 50%;
+	transform: translateX(-50%);
+	
+}
+.modal-out{
+	left: 0;
+	transform: translateX(-100%);
+}
+
+</style>
---
+++ components/PersonalModal/modal.js
@@ -0,0 +1,33 @@
+//引入你要挂载的组件
+import modal from './PersonalModal.vue'
+
+
+export default {
+	//实现vue安装插件的方法
+	install(Vue){
+		const personalModal = Vue.extend(modal)
+		
+		const instance = new personalModal()
+		
+		//挂载
+		instance.vm = instance.$mount()
+		
+		
+		//将元素添加到body中
+		document.body.appendChild(instance.vm.$el)
+		
+		const modalEl = document.getElementsByClassName('personal-modal').item(0)
+		
+		
+		
+		Vue.prototype.$modalIn = ()=>{
+			modalEl.className = 'personal-modal modal-in'
+		}
+		
+		
+		Vue.prototype.$modalOut = ()=>{
+			modalEl.className = 'personal-modal modal-out'
+		}
+		
+	}
+}
