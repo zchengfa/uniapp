926acc59ea7d7141d490e5378f24d94d950bce1b
zchengfa <1522977814@qq.com>
2022-12-09 12:21:07 +0800

    finish recentlyPlayed page

--- pages/mineMenu/recentlyPlayed/recentlyPlayed.vue
+++ pages/mineMenu/recentlyPlayed/recentlyPlayed.vue
@@ -11,10 +11,10 @@
 				</view>
 			</view>
 		</scroll-view>
-		<swiper class="swiper-scroll" :style="scrollHeightSwiper" :indicator-dots="false" :autoplay="false"  :duration="300" @change="changeItem" :current="currentIndex">
+		<swiper class="swiper-scroll" :style="scrollHeightSwiper" :indicator-dots="false" :autoplay="false"  :duration="300" @change="changeRecently" :current="currentIndex">
 			<!-- 最近歌曲 -->
 			<swiper-item>
-				<scroll-view scroll-y="true" class="scroll-v swiper-item-scroll list-scroll" :style="scrollHeightSwiper"  @scrolltolower="loadMore">
+				<scroll-view scroll-y="true" class="scroll-v swiper-item-scroll list-scroll" :style="scrollHeightSwiper"  @scrolltolower="loadMore(menu[0].type)">
 					<view class="list-item" v-for="(item,index) in menu[0].showData" :key="item.data.id">
 						<image src="../../../static/images/playing.png" mode="aspectFit" v-if="songId === item.data.id" class="playing"></image>
 						<view class="item-info">
@@ -38,26 +38,110 @@
 			</swiper-item>
 			<!-- 最近视频 -->
 			<swiper-item>
-				<scroll-view scroll-y="true" class="scroll-v swiper-item-scroll list-scroll" :style="scrollHeightSwiper" @scrolltolower="loadMore">
+				<scroll-view scroll-y="true" class="scroll-v swiper-item-scroll list-scroll" :style="scrollHeightSwiper" @scrolltolower="loadMore(menu[1].type)">
 					<view class="sheets-item" v-for="(item,index) in menu[1].showData" :key="index" @tap="toVideoDetail(item.data.id)">
 						<view class="image-box">
 							<image :src="item.data.coverUrl" mode="aspectFill" class="sheet-image"></image>
 						</view>
 						<view class="info">
 							<view class="title-box">
-								<text v-if="item.resourceType === 'MV'" class="mv">MV</text>
-								<text class="video-title">{{item.resourceType === 'MLOG'? item.data.title : item.data.name}}</text>
+								<text v-if="item.resourceType === 'MV'" class="mv-tag">MV</text>
+								<text class="sheet-title" :class="{'mv-show':item.resourceType === 'MV'}">{{item.resourceType === 'MLOG'? item.data.title : item.data.name}}</text>
 							</view>
 							<view class="creator">
-								<text class="time">{{$transTime(item.data.duration/1000)}}</text>
-								<text class="ar" v-if="item.data.hasOwnProperty('artists')">{{$dealAuthor(item.data.artists,'name')}}</text>
-								<text class="nick-name" v-if="item.data.hasOwnProperty('creator')">by {{item.data.creator.nickname}}</text>
+								<view class="creator-left">
+									<text class="time">{{$transTime(item.data.duration/1000)}}</text>
+									<text class="ar" v-if="item.data.hasOwnProperty('artists')">{{$dealAuthor(item.data.artists,'name')}}</text>
+									<text class="nick-name" v-if="item.data.hasOwnProperty('creator')">by {{item.data.creator.nickname}}</text>
+								</view>
+								<text class="play-time">{{item.playTime}}</text>
 							</view>
 						</view>
 					</view>
 				</scroll-view>
 			</swiper-item>
-			
+			<!-- 最近声音 -->
+			<swiper-item>
+				<scroll-view scroll-y="true" class="scroll-v swiper-item-scroll list-scroll" :style="scrollHeightSwiper" @scrolltolower="loadMore(menu[2].type)">
+					<view class="sheets-item" v-for="(item,index) in menu[2].showData" :key="index" @tap="toVideoDetail(item.data.id)">
+						<view class="voice-image-box image-box">
+							<image :src="item.data.pubDJProgramData.coverUrl" mode="aspectFill" class="voice-image"></image>
+						</view>
+						<view class="info">
+							<view class="title-box">
+								<text class="sheet-title">{{item.data.pubDJProgramData.name}}</text>
+							</view>
+							<view class="creator voice-creator">
+								<image src="../../../static/images/time.png" mode="aspectFit" class="time-image"></image>
+								<text>{{$transTime(item.data.pubDJProgramData.duration/1000)}}</text>
+							</view>
+						</view>
+					</view>
+				</scroll-view>
+			</swiper-item>
+			<!-- 最近歌单 -->
+			<swiper-item>
+				<scroll-view scroll-y="true" class="scroll-v swiper-item-scroll list-scroll" :style="scrollHeightSwiper" @scrolltolower="loadMore(menu[3].type)">
+					<view class="sheets-item" v-for="(item,index) in menu[3].showData" :key="index" @tap="toVideoDetail(item.data.id)">
+						<view class="voice-image-box image-box">
+							<image :src="item.data.coverImgUrl" mode="aspectFill" class="voice-image"></image>
+						</view>
+						<view class="info playlist-info">
+							<view class="title-box">
+								<text class="sheet-title">{{item.data.name}}</text>
+							</view>
+							<view class="creator">
+								<view class="creator-left">
+									<text class="nick-name">by {{item.data.creator.nickname}}</text>
+								</view>
+								<text class="play-time">{{item.playTime}}</text>
+							</view>
+						</view>
+					</view>
+				</scroll-view>
+			</swiper-item>
+			<!-- 最近专辑 -->
+			<swiper-item>
+				<scroll-view scroll-y="true" class="scroll-v swiper-item-scroll list-scroll" :style="scrollHeightSwiper" @scrolltolower="loadMore(menu[4].type)">
+					<view class="sheets-item" v-for="(item,index) in menu[4].showData" :key="index" @tap="toVideoDetail(item.data.id)">
+						<view class="voice-image-box image-box">
+							<image :src="item.data.picUrl" mode="aspectFill" class="voice-image"></image>
+						</view>
+						<view class="info playlist-info">
+							<view class="title-box">
+								<text class="sheet-title">{{item.data.name}}</text>
+							</view>
+							<view class="creator">
+								<view class="creator-left">
+									<text class="nick-name">{{$dealAuthor(item.data.artists,'name')}}</text>
+								</view>
+								<text class="play-time">{{item.playTime}}</text>
+							</view>
+						</view>
+					</view>
+				</scroll-view>
+			</swiper-item>
+			<!-- 最近播客 -->
+			<swiper-item>
+				<scroll-view scroll-y="true" class="scroll-v swiper-item-scroll list-scroll" :style="scrollHeightSwiper" @scrolltolower="loadMore(menu[5].type)">
+					<view class="sheets-item" v-for="(item,index) in menu[5].showData" :key="index" @tap="toVideoDetail(item.data.id)">
+						<view class="voice-image-box image-box">
+							<image :src="item.data.picUrl" mode="aspectFill" class="voice-image"></image>
+						</view>
+						<view class="info playlist-info">
+							<view class="title-box">
+								<text class="sheet-title">{{item.data.name}}</text>
+							</view>
+							<view class="creator">
+								<view class="creator-left">
+									<text class="nick-name">by {{item.data.dj.nickname}}</text>
+								</view>
+								<text class="play-time">{{item.playTime}}</text>
+							</view>
+						</view>
+					</view>
+				</scroll-view>
+			</swiper-item>
 		</swiper>
 		<!-- 底部音乐控制 -->
 		<view class="bottom-control" v-show="isShowBottomControl" >
@@ -75,6 +159,7 @@
 	import { recentlyPlayed } from '@/common/api.js'
 	import { bottomControlMixin , playSongMixin} from '@/common/mixins/mixins.js'
 	import { mapGetters } from 'vuex'
+	import { timeFormatting , transTime } from '@/utils/utils.js'
  	
 	export default {
 		mixins:[bottomControlMixin,playSongMixin],
@@ -132,27 +217,72 @@
 			...mapGetters(['songId'])
 		},
 		methods: {
+			changeRecently(e){
+				this.into = 't' + e.detail.current
+				this.currentIndex = e.detail.current
+			},
 			selectTag(into,index){
 				this.currentIndex = index
 				this.into = into
 			},
 			getData(){
+				uni.showLoading({
+					title:'加载中...'
+				})
 				this.menu.map(item =>{
 					
 					recentlyPlayed(item.type).then(res=>{
 						//console.log(res)
 						if(res.code === 200) {
 							item.totalData.push(...res.data.list)
+							if(item.type === 'video'){
+								this.dealShowTime(this.menu[1].totalData)
+							}
+							else if(item.type === 'playlist'){
+								this.dealShowTime(this.menu[3].totalData)
+							}
+							else if(item.type === 'album'){
+								this.dealShowTime(this.menu[4].totalData)
+							}
+							else if(item.type === 'dj'){
+								this.dealShowTime(this.menu[5].totalData)
+							}
 							item.showData.push(...item.totalData.slice(0,30))
+							
 						}
 					})
 				})
-			},
-			changeItem(){
 				
 			},
-			loadMore(){
-				
+			dealShowTime(arr){
+				//处理时间
+				arr.map(item=>{
+					item.playTime = timeFormatting('YYYY年M月D日',item.playTime)
+					//获取今年的年份
+					let year = new Date().getFullYear()
+					let itemYear = Number(item.playTime.substr(0,4))
+					
+					//如果播放时间是在今年，只显示月日
+					year === itemYear ? item.playTime = item.playTime.substring(5,item.playTime.length) : null
+				})
+			},
+			loadMore(type){
+				uni.showLoading({
+					title:'加载更多'
+				})
+				this.menu.map(item=>{
+					if(item.type === type){
+						let currentLength = item.showData.length
+						let totalLength = item.totalData.length
+						if(currentLength < totalLength){
+							item.showData.push(...item.totalData.slice(currentLength,currentLength + 30))
+						}
+						let timer = setTimeout(()=>{
+							uni.hideLoading()
+							clearTimeout(timer)
+						},1000)
+					}
+				})
 			},
 			//整理播放列表所需要的数据
 			getPlayListData(){
@@ -166,13 +296,27 @@
 		},
 		created() {
 			this.getData()
+		},
+		mounted() {
+			let timer = setTimeout(()=>{
+				uni.hideLoading()
+				clearTimeout(timer)
+			},1000)
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
+	.time-image{
+		margin-right: 2px;
+		width: 12px;
+		height: 12px;
+	}
 	.menu-active{
 		max-width: 28px;
+	}
+	.mv-show{
+		text-indent: 30px;
 	}
 .scroll-x{
 		margin: 0 auto;
@@ -207,42 +351,63 @@
 	width: 94%;
 }	
 .sheets-item{
-		display: flex;
-		justify-content: flex-start;
-		align-items: center;
-		margin-top: 10px;
+		position: relative;
+		height: 80px;
 	}
 	.sheet-image{
+		position: absolute;
+		top:50%;
 		width: 80px;
-		height: 60px;
+		height: 64px;
 		border-radius: 6px;
+		transform: translateY(-50%);
+	}
+	.image-box{
+		width: 80px;
+		height: 100%;
+	}
+	.image-box,.info{
+		display: inline-block;
 	}
 	.info{
+		position: absolute;
+		top:50%;
+		width: 75%;
 		margin-left: 10px;
-		.video-title{
-			margin: 10px  10px 10px 0;
+		transform: translateY(-50%);
+		.sheet-title{
+			margin: 0  0 10px 0;
 			display: inline-block;
+			max-width: 240px;
 			font-size: 14px;
 			white-space: pre-wrap;
+			text-align: left;
 		}
 		.art-item{
 			margin-right: 10px;
 			color: #767676;
 		}
 	}
-	.image-box{
-		position: relative;
-	}
+	
 	.creator{
 		color: #ababab;
 		font-size: 12px;
+		.creator-left{
+			display: inline-block;
+			width: 70%;
+		}
 		.time{
 			margin-right: 8px;
 		}
+		.play-time{
+			display: inline-block;
+			width: 30%;
+			text-align: right;
+		}
 	}
-	.mv{
-		display: inline-block;
-		margin-right: 0;
+	
+	.mv-tag{
+		position: absolute;
 		padding: 2px 6px;
 		border: 1px solid #ff8ec1;
 		color: #f30000;
@@ -250,6 +415,28 @@
 		border-radius: 4px;
 		transform-origin: 0;
 		transform: scale(.7);
+	}
+	.voice-creator{
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+	}
+	.voice-image-box{
+		position: relative;
+		top: 50%;
+		width: 50px;
+		height: 64px;
+		transform: translateY(-50%);
+	}
+	.voice-image{
+		position: absolute;
+		top:7px;
+		width: 50px;
+		height: 50px;
+		border-radius: 6px;
+	}
+	.playlist-info{
+		width: 84%;
 	}
 .bottom-control{
 	bottom: 0;
