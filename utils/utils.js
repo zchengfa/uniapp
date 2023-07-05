ed0bf68f7387e8c871ef30c09e584a2df6a3b621
zchengfa <1522977814@qq.com>
2023-07-05 13:57:01 +0800

    给api.js文件生成注释文档

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,9 @@
-//处理播放量
+/**
+ * @param { Number } count 数量
+ * @return { Number } 返回处理过的数量
+ * @example
+	dealCount(100000)
+ */
 export function dealCount ( count ) {
 	if(count >=10000 && count < 100000000){
 		return (count/10000).toFixed(2) + '万'
@@ -12,17 +17,30 @@ export function dealCount ( count ) {
 }
 
 
-//处理歌曲作者
-export function dealAuthor ( authorArr = [] , arrProperty) {
+/**
+ * @param { Array } authorArr 数组
+ * @param { String } arrProperty 数组中要处理的属性
+ * @param { String } splitChar 分割符 
+ * @return { Array } 返回处理完成的数组
+ * @example 
+	dealAuthor(arr,'name','/')
+ */
+export function dealAuthor ( authorArr = [] , arrProperty, splitChar = '/') {
 	let author = ''
 	let num = 0
 	authorArr.map(item=>{
-		author += item[arrProperty]+'/'
+		author += item[arrProperty]+splitChar
 	})
 	return author.substr(0,author.length-1)
 }
 
-//节流
+/**
+ * @param { Function } func 执行函数
+ * @param { Number } delay 执行时间
+ * @return { Function } 返回节流函数
+ * @example 
+   throttle(func,3000)
+ */
 export function throttle (func,delay) {
     let flag = true
     return function (...args) {
@@ -36,7 +54,12 @@ export function throttle (func,delay) {
     }
 }
 
-//根据VIP等级返回大写的等级
+/**
+ * @param { Number } num 数字（10以内）
+ * @return { String } 返回大写的数字
+ * @example 
+	level(8)
+ */
 export function level(num){
 	
 	switch(num){
@@ -75,7 +98,13 @@ export function level(num){
 	}
 }
 
-//时间格式化
+/**
+ * @param { String } fm 时间格式
+ * @param { Number } time 时间
+ * @return {String} 返回处理后的时间
+ * @example 
+	timeFormatting('YYYY-MM-DD hh-mm-ss',134566534536)
+ */
 export function timeFormatting (fm,time){
     //拓展Date的时间格式化函数
     Date.prototype.format = function (fmt){
@@ -120,7 +149,10 @@ export function timeFormatting (fm,time){
 }
 
 
-//时间转换成分钟
+/**
+ * @param { Number } target 时间
+ * @return { String } 返回处理过的时间
+ */
 export function transTime(target){
 	let minutes = target/60
 	let m = dealTime(Math.floor(minutes))
@@ -136,7 +168,14 @@ export function transTime(target){
 	return t
 }
 
-
+/**
+ * @param { Array } arr 要添加颜色属性的数组
+ * @param { String } property 颜色属性
+ * @param { Array } colors 自定义的颜色数组 
+ * @return { Array } 返回带有颜色属性的数组
+ * @example 
+	 getColor(arr,'bgColor',['#fff','000'])
+ */
 export function getColor(arr,property,colors = []){
 	if(!colors.length){
 		colors = ['#633000','#415b63','#271200','#632953','#32f363','#ffaaff']
